a76c7519771356fdb2bb73b3fa4bb7a5c319d4b7
darwinknowless <id.darwins@gmail.com>
2021-07-07 16:21:56 +0700

    rewrite project

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const c = require("config");
 const express = require("express");
+const connectDB = require("./config/database");
+
 const app = express();
 
+//Connect database
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
