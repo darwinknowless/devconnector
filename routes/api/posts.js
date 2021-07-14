6584fa86d17a1cfc14849c8be232ca8b36edfb77
darwinknowless <id.darwins@gmail.com>
2021-07-14 22:18:34 +0700

    ADD : 5, COR : 25, Title : Add Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middlewares/auth");
 
-// @route       GET api/posts
-// @desc        Test route
-// @access      public
-router.get("/", (req, res) => res.send("Post route"));
+// Import model
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
+
+// @route       POST api/posts
+// @desc        Create a post
+// @access      private
+router.post(
+	"/",
+	[auth, [check("text", " Text is required").not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			// Find user
+			const user = await User.findById(req.user.id).select("-password");
+			// Build pst object
+			const newPost = new Post({
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			});
+			// Create post
+			const post = await newPost.save();
+			// Result/return
+			res.json(post);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
 
 module.exports = router;
